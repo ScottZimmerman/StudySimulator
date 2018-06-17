fc90a6514deffae90c05a56b53942433923e82d5
Scott Zimmerman <ScottZimmerman@users.noreply.github.com>
2018-06-16 23:57:29 -0700

    Update MinTxProp.R

--- Features/MinTxProp.R
+++ Features/MinTxProp.R
@@ -1,6 +1,8 @@
 function(outcome,exposure){
 	f <- function(name,options){
 		tempTable <- pXgivenPa[[exposure]]
+		print(tempTable)
+		q("no")
 		#print("Calculating minimum treatment propensity")
 		query <- "select min(P) as min_P from tempTable"
 		features[[name]] <<- sqldf(query)[,"min_P"]
